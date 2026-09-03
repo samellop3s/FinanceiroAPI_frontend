c6c1fc90aeb90f57be81370d80c188a2b77437a6
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-09-03 15:41:58 -0300

    fix : correção na chave API

--- js/listagem.js
+++ js/listagem.js
@@ -1,4 +1,4 @@
-const URL_API = "https://financeiro-api-lopes-h7hcgub8f3aggmdn.centralus-01.azurewebsites.net/api/Pagamentos";
+const urlDownload = `https://financeiro-api-lopes-h7hcgub8f3aggmdn.centralus-01.azurewebsites.net/api/Pagamentos/anexo/${encodeURIComponent(nomeArquivo)}`;
 
 async function carregarPagamentos() {
     const corpoTabela = document.getElementById("corpoTabela");
